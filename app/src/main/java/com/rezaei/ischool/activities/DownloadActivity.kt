91e2de4c2cd02f59a01e5b3da48bf7495fa38842
rhmtrz <rezaeihazara@gmail.com>
2023-07-30 19:32:52 +0900

    feat: setup material 3

--- app/src/main/java/com/rezaei/ischool/activities/DownloadActivity.kt
+++ app/src/main/java/com/rezaei/ischool/activities/DownloadActivity.kt
@@ -8,7 +8,6 @@ import com.rezaei.ischool.R
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.ktx.storage
-import com.rezaei.ischool.MainActivity
 import com.google.firebase.storage.ktx.component1
 
 class DownloadActivity : AppCompatActivity() {
